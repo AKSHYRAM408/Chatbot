006672942339f5028f491d44ea84821929aac861
AKSHYRAM M <137417218+AKSHYRAM408@users.noreply.github.com>
2025-02-10 17:15:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -208,8 +208,8 @@ def main():
 
     # Define the file paths you want to process
     file_paths = [
-        "C:\\Users\\Dell\\Downloads\\ak1.xlsx",  # Path to your Excel file
-        "C:\\Users\\Dell\\Downloads\\Full Stack (1).pdf",  # Path to your PDF file
+        "Documents\\ak1.xlsx",  # Path to your Excel file
+        "Documents\\Zoho Resume Manjineshwaran.pdf",  # Path to your PDF file
     ]
     
     # URL to be processed
